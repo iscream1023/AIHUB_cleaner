d797feb7430267bd804e3cf5f9687f716b7e7666
haggi_Na <haggi7289@gmail.com>
2026-02-19 13:33:35 +0900

    delete #

--- init.py
+++ init.py
@@ -15,21 +15,21 @@ class AIHubPipeline:
         print("🔥 AI-HUB datasets cleaner ACTIVATED ")
         
         # Step 1: clean folder structure
-        #fm = FolderManager(src_root=self.cfg['path']['src_root'], 
-                           #dst_root=self.cfg['path']['dst_root'])
-        #fm.collect(rename_with_parent=True)
+        fm = FolderManager(src_root=self.cfg['path']['src_root'], 
+                           dst_root=self.cfg['path']['dst_root'])
+        fm.collect(rename_with_parent=True)
 
         # Step 2: D-Hashing init
-        #dedup = Deduplicator(img_dir=fm.dst_img_dir, 
-                             #lbl_dir=fm.dst_lbl_dir, 
-                             #hash_size=self.cfg['dedup'].get('hash_size', 8))
+        dedup = Deduplicator(img_dir=fm.dst_img_dir, 
+                             lbl_dir=fm.dst_lbl_dir, 
+                             hash_size=self.cfg['dedup'].get('hash_size', 8))
         
         # D-HASHING threshold 
-        #dedup.run(threshold=self.cfg['dedup'].get('threshold', 2))
+        dedup.run(threshold=self.cfg['dedup'].get('threshold', 2))
 
         # Step 3: CONVERT INTO YOLO FORMAT
-        #converter = Converter(config_path=self.config_path)
-        #converter.process_all()
+        converter = Converter(config_path=self.config_path)
+        converter.process_all()
 
         # 4. Sampling & YAML Generation (추가된 부분)
         sampler = Sampler(self.cfg)
